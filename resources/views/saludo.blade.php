6fe15584507c7c3fb37a5bb76d34da1926772364
Ekain Susperregi Ibarguren <suspekain@gmail.com>
2019-10-11 09:55:25 +0200

    Lehen aldaketak

---
+++ resources/views/saludo.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Saludo</title>
+  </head>
+  <body>
+    <h1>Kaixo denei!</h1>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('saludo', function () {
+    return view('saludo');
+})->name('saludo');
